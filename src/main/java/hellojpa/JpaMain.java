821b3d135160e07437103d0017f58ae6d7a30c6c
ljh0326 <hun2@tossinsu.com>
2020-04-05 17:55:53 +0900

    1. 간단한 실행  - JPA실행은 트렌젝션 안에서해야함  - 엔티티 매니저 팩토리는 디비당 하나  - 엔티티 매니저는 연결당 하나 빠르게 쓰고 close해야해  - dialect를 설정하면 jpql생성시 그 방언에 관련된 커리를 만들어줌

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -13,14 +13,25 @@ public class JpaMain {
         tx.begin();
         //실제 코드 작성하는곳 , jpa트렌젝션 단위엄청좋아해, jpa변경관련된거 다 트렌젝션 안에서 진행해야
 
-        Member member = new Member();
+        //스프링과 함께쓰면 이걸 에러케치를 다 해줌
+        try{
+            //삽입
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("hello");
+            em.persist(member);
 
-        member.setId(1L);
-        member.setName("hello");
+            //찾기
+            Member findMember = em.find(Member.class, 1L);
 
-        em.persist(member);
-        tx.commit();
-        em.close();
-        emf.close();
+            //삭제
+            em.remove(member);
+            tx.commit();
+        }catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
